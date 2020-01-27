8ba059d469d7bd15e837f9d2defbf2f126e86d1e
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-27 19:22:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,12 +13,13 @@ module FormsLab
     
     post '/pirates' do
     @pirate = Pirate.new(params[:pirate])
- 
+    
     params[:pirate][:ships].each do |details|
-    Ship.new(details)
+      Ship.new(details)
     end
  
     @ships = Ship.all
+    binding.pry
  
     erb :'/pirates/show'
     end
